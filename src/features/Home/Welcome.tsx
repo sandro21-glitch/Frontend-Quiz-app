389f61883c9914d920db74b98feb4c0122d1647f
Sandro <vipsandro11@gmail.com>
2023-12-29 16:24:27 +0400

    style: change welcome component style

--- src/features/Home/Welcome.tsx
+++ src/features/Home/Welcome.tsx
@@ -1,9 +1,10 @@
 const Welcome = () => {
   return (
-    <div>
-      <h1>
+    <div className="md:flex-1">
+      <h1 className="mb-10">
         Welcome to the <br /> <span className="font-bold">Frontend Quiz!</span>
       </h1>
+      <p className="italic mb-10 md:mb-0">Pick a subject to get started.</p>
     </div>
   );
 };
