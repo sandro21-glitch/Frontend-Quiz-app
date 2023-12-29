4b92da7df3aa41a096de5b3dace5ba502f2980f3
Sandro <vipsandro11@gmail.com>
2023-12-29 16:24:44 +0400

    style: change homepage style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import Subjects from "../features/Home/Subjects";
 import Welcome from "../features/Home/Welcome";
 
 const Home = () => {
   return (
-    <section className="section-center" style={{ marginTop: "10rem" }}>
+    <section
+      className="section-center flex items-start flex-col md:flex-row"
+      style={{ marginTop: "5rem" }}
+    >
       <Welcome />
+      <Subjects />
     </section>
   );
 };
