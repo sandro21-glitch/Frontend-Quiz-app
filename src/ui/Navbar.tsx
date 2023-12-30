ad13546c62abc7752e434f3b1c3a0795f6f8b813
Sandro <vipsandro11@gmail.com>
2023-12-30 17:23:02 +0400

    feat: Implement Dark Mode Icons and Theme Switcher

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,22 +1,22 @@
 import sunIcon from "/icon-sun-dark.svg";
 import moonIcon from "/icon-moon-dark.svg";
+import sunIconLight from "/icon-sun-light.svg";
+import moonIconLight from "/icon-moon-light.svg";
+import ThemeSwitcher from "./ThemeSwitcher";
+import { useAppSelector } from "../hooks/reduxHooks";
 const Navbar = () => {
+  const darkMode = useAppSelector((store) => store.home.darkMode);
   return (
-    <header className="section-center" style={{ marginTop: "5rem" }}>
+    <header className="section-center pt-0" style={{ paddingTop: "5rem" }}>
       <nav className="flex-between">
-        <img src="" alt="logo" />
+        <img src="" alt="logo" className="invisible" />
         <ul className="flex gap-5">
           <li>
-            <img src={sunIcon} alt="sun" />
+            <img src={darkMode ? sunIconLight : sunIcon} alt="sun" />
           </li>
+          <ThemeSwitcher />
           <li>
-            <label className="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="w-11 h-6 bg-medium-purple peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-medium-purple"></div>
-            </label>
-          </li>
-          <li>
-            <img src={moonIcon} alt="moon" />
+            <img src={darkMode ? moonIconLight : moonIcon} alt="moon" />
           </li>
         </ul>
       </nav>
