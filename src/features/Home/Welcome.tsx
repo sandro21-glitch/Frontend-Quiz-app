97a7d43c415f32ce7f35f94dd2bf3240330c2de5
Sandro <vipsandro11@gmail.com>
2023-12-30 17:22:21 +0400

    style: Add Dark Mode Styling to Welcome Component

--- src/features/Home/Welcome.tsx
+++ src/features/Home/Welcome.tsx
@@ -1,7 +1,12 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
+
 const Welcome = () => {
+  const darkMode = useAppSelector((store) => store.home.darkMode);
   return (
     <div className="md:flex-1">
-      <h1 className="mb-10">
+      <h1
+        className={`${darkMode ? "text-white" : "text-color"} mb-10 font-light`}
+      >
         Welcome to the <br /> <span className="font-bold">Frontend Quiz!</span>
       </h1>
       <p className="italic mb-10 md:mb-0">Pick a subject to get started.</p>
