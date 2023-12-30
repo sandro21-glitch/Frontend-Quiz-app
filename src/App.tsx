9a48d3bc87b532da7ca6c81d990263057e987274
Sandro <vipsandro11@gmail.com>
2023-12-30 17:17:57 +0400

    feat: Add Body Background Component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./ui/Navbar";
+import BodyBg from "./ui/BodyBg";
 
 function App() {
   return (
     <BrowserRouter>
+      <BodyBg />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
