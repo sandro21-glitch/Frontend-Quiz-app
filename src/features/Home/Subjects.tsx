cbdcdc426a64cb035804f8c348865c0651b829e8
Sandro <vipsandro11@gmail.com>
2023-12-30 17:22:08 +0400

    refactor: Implement Dynamic Rendering of Subjects

--- src/features/Home/Subjects.tsx
+++ src/features/Home/Subjects.tsx
@@ -1,45 +1,36 @@
-import htmlLogo from "/icon-html.svg";
-import cssLogo from "/icon-css.svg";
-import jsLogo from "/icon-js.svg";
-import accessibilityLogo from "/icon-accessibility.svg";
+import { subjectsList } from "../../constants/subjecList";
+import { useAppSelector } from "../../hooks/reduxHooks";
 const Subjects = () => {
+  const darkMode = useAppSelector((store) => store.home.darkMode);
   return (
     <section className="md:flex-1 w-full">
       <ul className="w-full flex flex-col gap-5 items-start">
-        <li className=" bg-white p-5 rounded-[1rem] w-full text-left flex items-center gap-5 cursor-pointer shadow-sm">
-          <img
-            src={htmlLogo}
-            alt="html logo"
-            className="bg-html-bg w-[2.5rem] h-[2.5rem] p-1 rounded-[.3rem]"
-          />
-          <p className="font-bold text-[1.5rem] text-text-color">HTML</p>
-        </li>
-        <li className=" bg-white p-5 rounded-[1rem] w-full text-left flex items-center gap-5 cursor-pointer shadow-sm">
-          <img
-            src={cssLogo}
-            alt="css logo"
-            className="bg-css-bg w-[2.5rem] h-[2.5rem] p-1 rounded-[.3rem]"
-          />
-          <p className="font-bold text-[1.5rem] text-text-color">CSS</p>
-        </li>
-        <li className=" bg-white p-5 rounded-[1rem] w-full text-left flex items-center gap-5 cursor-pointer shadow-sm">
-          <img
-            src={jsLogo}
-            alt="js logo"
-            className="bg-js-bg w-[2.5rem] h-[2.5rem] p-1 rounded-[.3rem]"
-          />
-          <p className="font-bold text-[1.5rem] text-text-color">JavaScript</p>
-        </li>
-        <li className=" bg-white p-5 rounded-[1rem] w-full text-left flex items-center gap-5 cursor-pointer shadow-sm">
-          <img
-            src={accessibilityLogo}
-            alt="accessibility logo"
-            className="bg-accessibility-bg w-[2.5rem] h-[2.5rem] p-1 rounded-[.3rem]"
-          />
-          <p className="font-bold text-[1.5rem] text-text-color capitalize">
-            Accessibility
-          </p>
-        </li>
+        {subjectsList.map((sub) => {
+          const { id, subject, logo, bgColor } = sub;
+          console.log(bgColor);
+          return (
+            <li
+              key={id}
+              className={`${
+                darkMode ? "bg-darkBg" : "bg-white"
+              } p-5 rounded-[1rem] w-full text-left flex items-center gap-5 cursor-pointer shadow-sm`}
+            >
+              <img
+                src={logo}
+                alt={`${subject} logo`}
+                style={{ backgroundColor: bgColor }}
+                className="w-[2.5rem] h-[2.5rem] p-1 rounded-[.3rem]"
+              />
+              <p
+                className={`font-bold text-[1.5rem] ${
+                  darkMode ? "text-white" : "text-text-color"
+                }`}
+              >
+                {subject}
+              </p>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
