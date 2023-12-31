cbcd720d2ac988cc98446762f2a202c6194652c9
Sandro <vipsandro11@gmail.com>
2023-12-31 21:44:29 +0400

    feat: Implement conditional rendering for quizzes in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,20 @@
 import Subjects from "../features/Home/Subjects";
 import Welcome from "../features/Home/Welcome";
+import QuizStart from "../features/Quizzes/QuizStart";
+import { useAppSelector } from "../hooks/reduxHooks";
 
 const Home = () => {
+  const isActiveQuiz = useAppSelector((store) => store.quiz.isActiveQuiz);
   return (
-    <section
-      className="section-center flex items-start flex-col md:flex-row"
-      style={{ marginTop: "5rem" }}
-    >
-      <Welcome />
-      <Subjects />
+    <section className="section-center " style={{ marginTop: "5rem" }}>
+      {!isActiveQuiz ? (
+        <div className="flex items-start flex-col md:flex-row">
+          <Welcome />
+          <Subjects />
+        </div>
+      ) : (
+        <QuizStart />
+      )}
     </section>
   );
 };
