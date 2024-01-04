6763ca627fb87f4df41d10c8f8086aed1c29177e
Sandro <vipsandro11@gmail.com>
2024-01-04 14:09:25 +0400

    refactor: export generateLetters function

--- src/features/Quizzes/utils/GenerateLetters.ts
+++ src/features/Quizzes/utils/GenerateLetters.ts
@@ -1,4 +1,4 @@
-const generateLetters = (numOptions: number): string[] => {
+export const generateLetters = (numOptions: number): string[] => {
   const letters = [];
   for (let i = 0; i < numOptions; i++) {
     const letter = String.fromCharCode(65 + i);
