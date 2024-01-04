ccb591278d92e1c6604489a9bec4be2f486c5f45
Sandro <vipsandro11@gmail.com>
2024-01-04 19:54:28 +0400

    feat: Add route for finishing quiz

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./ui/Navbar";
 import BodyBg from "./ui/BodyBg";
+import FinishQuiz from "./pages/FinishQuiz";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/finished" element={<FinishQuiz />} />
       </Routes>
     </BrowserRouter>
   );
