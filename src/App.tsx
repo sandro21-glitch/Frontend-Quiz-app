bdf45d59214d02f274f9a0bc26ed6acde9fdacf3
Sandro <vipsandro11@gmail.com>
2024-01-05 00:16:26 +0400

    refactor: :sparkles: Use ProtectedRoute for '/finished' route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+// App.tsx
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./ui/Navbar";
 import BodyBg from "./ui/BodyBg";
 import FinishQuiz from "./pages/FinishQuiz";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 function App() {
   return (
@@ -11,7 +13,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/finished" element={<FinishQuiz />} />
+        <Route
+          path="/finished"
+          element={
+            <ProtectedRoute>
+              <FinishQuiz />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
