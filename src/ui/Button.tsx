fb3b4df4b96ec585ccf8dae67e22de09a84a6637
Sandro <vipsandro11@gmail.com>
2024-01-05 15:28:18 +0400

    style: Enhance Button component responsiveness

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -6,7 +6,8 @@ const Button = ({ type, func }: ButtonTypes) => {
   return (
     <button
       onClick={func}
-      className="mt-7 w-full bg-medium-purple font-bold text-white text-[2rem] p-[1.5rem] rounded-3xl mb-7"
+      className="mt-7 w-full bg-medium-purple font-bold text-white
+       text-[1.3rem] md:text-[2rem] p-[.5rem] md:p-[1.5rem] rounded-xl md:rounded-3xl mb-7"
     >
       {type}
     </button>
