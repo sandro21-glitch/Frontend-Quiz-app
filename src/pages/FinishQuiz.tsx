f38d56f0320143abf776339cc42d11152b9384d7
Sandro <vipsandro11@gmail.com>
2024-01-05 15:28:05 +0400

    style: Update FinishQuiz component UI for dark mode support

--- src/pages/FinishQuiz.tsx
+++ src/pages/FinishQuiz.tsx
@@ -5,6 +5,7 @@ import Button from "../ui/Button";
 import { resetQuiz } from "../features/Quizzes/quizSlice";
 const FinishQuiz = () => {
   const { icon, title, score } = useAppSelector((store) => store.quiz);
+  const darkMode = useAppSelector((store) => store.home.darkMode);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const resetQuizData = () => {
@@ -14,20 +15,38 @@ const FinishQuiz = () => {
   return (
     <section className="section-center " style={{ marginTop: "5rem" }}>
       <article className="flex items-start flex-col md:flex-row">
-        <h1 className="flex-1">
+        <h1
+          className={`w-full mb-5 md:flex-1 ${
+            darkMode ? "text-white" : "text-text-color"
+          }`}
+        >
           Quiz completed <br />{" "}
           <span className="font-semibold">You scored...</span>
         </h1>
         {/* quiz statistic */}
-        <div className="flex-1">
-          <div className="px-5 py-10 bg-white  rounded-3xl flex justify-center items-center flex-col">
-            <div className="flex items-center gap-7 mb-14">
+        <div className="w-full md:flex-1">
+          <div
+            className={`px-5 py-10 ${
+              darkMode ? "bg-darkBg" : "bg-white"
+            }   rounded-3xl flex justify-center items-center flex-col`}
+          >
+            <div className="flex items-center gap-3 md:gap-5 mb-14">
               <img src={icon} alt={`${title} logo`} className="visible" />
-              <h5 className="text-text-color font-medium">{title}</h5>
+              <h5
+                className={`${
+                  darkMode ? "text-white" : "text-text-color"
+                } font-medium`}
+              >
+                {title}
+              </h5>
             </div>
             {/* score */}
             <div className="flex flex-col items-center leading-[6rem]">
-              <p className="text-[10rem] font-semibold text-text-color mb-0">
+              <p
+                className={`text-[10rem] font-semibold ${
+                  darkMode ? "text-white" : "text-text-color"
+                } mb-0`}
+              >
                 {score}
               </p>
               <p className="mb-0 text-[1.5rem]">out of 10</p>
