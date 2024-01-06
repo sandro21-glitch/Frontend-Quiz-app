937577d60d6a24f6c7e76c35076e9d70cd316f28
Sandro <vipsandro11@gmail.com>
2024-01-06 16:43:19 +0400

    feat: :sparkles: Add dynamic background color to quiz icon in Navbar component

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -4,23 +4,25 @@ import sunIconLight from "/icon-sun-light.svg";
 import moonIconLight from "/icon-moon-light.svg";
 import ThemeSwitcher from "./ThemeSwitcher";
 import { useAppSelector } from "../hooks/reduxHooks";
+import { findSubjectColor } from "../utils/findSubColor";
 const Navbar = () => {
   const darkMode = useAppSelector((store) => store.home.darkMode);
   const icon = useAppSelector((store) => store.quiz.icon);
   const title = useAppSelector((store) => store.quiz.title);
   return (
-    <header className="section-center mx-auto mt-[2rem] lg:mt-[5rem]" >
+    <header className="section-center mx-auto mt-[2rem] lg:mt-[5rem]">
       <nav className="flex-between">
         <div className="flex items-center gap-7">
           <img
             src={icon}
             alt="logo"
-            className={`${icon ? "visible" : "invisible"}`}
+            className={`${icon ? "visible" : "invisible"} w-[2.5rem] h-[2.5rem] p-1`}
+            style={{ backgroundColor: findSubjectColor(title) }}
           />
           <h5
             className={`${
               darkMode ? "text-white" : "text-text-color"
-            } font-medium`}
+            } text-[1.8rem] font-medium`}
           >
             {title}
           </h5>
