59a6ef644680153510d8910ba89a274ee025f3e0
Sandro <vipsandro11@gmail.com>
2024-01-06 17:00:33 +0400

    initial commit

--- src/features/Quizzes/ErrorMsg.tsx
+++ src/features/Quizzes/ErrorMsg.tsx
@@ -2,7 +2,7 @@ import errorImage from "/icon-error.svg";
 
 const ErrorMsg = () => {
   return (
-    <div className="flex items-center justify-center text-[1rem] md:text-[1.5rem] text-red-bg">
+    <div className="flex items-center justify-center text-[1rem] md:text-[1.5rem] text-red-bg mb-5">
       <img src={errorImage} alt="error" />
       <span className="ml-4">Please select an answer</span>
     </div>
